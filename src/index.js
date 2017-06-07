d830fbf6f3c8718247c6b2e0ee24ff43e55f0581
Tiffany Lam <tlam5@sbcglobal.net>
2017-06-06 21:53:30 -0700

    Create a container component that is the parent component of the Editor and Previewer

--- src/index.js
+++ src/index.js
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom';
 import marked from 'marked';
 import './index.css';
 
+class Container extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markdown: ''
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Editor />
+        <Preview />
+      </div>
+    );
+  }
+}
+
 class Editor extends React.Component {
   render() {
     return (
@@ -17,14 +35,12 @@ class Preview extends React.Component {
   render() {
     return (
       <div className="preview">
+       <p></p>
       </div>
     );
   }
 }
 
 ReactDOM.render(
-  <div className="container">
-    <Editor />
-    <Preview />
-  </div>,
+  <Container />,
   document.getElementById('root'));
